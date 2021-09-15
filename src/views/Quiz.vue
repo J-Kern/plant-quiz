2733575950c34598219cfcc97f95bddb9e02d316
Joachim <kern_jo@web.de>
2021-09-15 21:02:14 +0200

    Merge Changes

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,30 +1,39 @@
 <template>
-    <div class="container mx-auto bg-white text-white">
+    <div class="container rounded-xl mx-auto my-6 flex flex-col items-center justify-center bg-white bg-glass-morphic text-black">
         <h1>Hello Quiz!</h1>
-        <router-link :to="{ name:'Question', params: { id: key }}">Hello World!</router-link>
-        <router-view v-model:key="plant.id" v-model:plant="plant"></router-view>
-      <div v-for="_plant in model">
-        <div>{{_plant.id}}</div>
-        <div>{{_plant.latinName}}</div>
-        <div>{{_plant.germanName}}</div>
-        <div>{{_plant.images[0]}}</div>
+        <router-link :to="{ name:'Question', params: { id: this.plant.id }}" :props="this.plant">Hello World!</router-link>
+        <router-view :props="this.plant"></router-view>
+      <div v-for="_plant in plants">
+        <div v-text="_plant.id"></div>
+        <div v-text="_plant.latinName"></div>
+        <div v-text="_plant.germanName"></div>
+        <div v-text="_plant.images[0]"></div>
       </div>
 
     </div>
 </template>
 
 <script>
+    import _plants from "@/data/plants";
+
     const staticData = require("@/data");
     export default {
       name: 'Quiz',
       props: [
           'plants'
       ],
-      // computed() {
-      //   return this.plants[Math.floor(Math.random()*this.plants.length)].id;
-      // },
-      // mounted() {
-      //   this.plant = this.plants[Math.floor(Math.random()*this.plants.length)];
-      // }
+      data() {
+        return {
+          plants: _plants,
+          plant: _plants[Math.floor(Math.random()*_plants.length)]
+        }
+      },
+      methods() {
+        return {
+          getRandomPlant: () => {
+            return this.data().plants[Math.floor(Math.random()*this.data().plants.length)];
+          }
+        }
+      }
     };
 </script>
